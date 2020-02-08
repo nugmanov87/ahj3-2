84e4997faa6faafcf081992ceb4e449d9cd603cd
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2020-02-08 10:21:41 +0500

    удален лишний класс

--- src/js/WorkTasks.js
+++ src/js/WorkTasks.js
@@ -1,23 +1,21 @@
 /* eslint-disable class-methods-use-this */
-import ArrData from './ArrData.js';
-import DisplayForm from './DisplayForm.js';
-import initialData from './InitialData.js';
-
+import { ArrData, initNotepad } from "./Init.js";
+import DisplayForm from "./DisplayForm.js";
 
 const arrData = new ArrData();
 const displayForm = new DisplayForm();
 
 export default class WorkTasks {
   constructor() {
-    this.divPined = document.getElementById('pined');
-    this.divAllTasks = document.getElementById('all-tasks');
-    this.formInput = document.getElementById('input-form');
-    this.elementInput = document.getElementById('input-task');
-    this.elementError = document.querySelector('.error');
+    this.pined = document.getElementById("pined");
+    this.allTasks = document.getElementById("all-tasks");
+    this.formInput = document.getElementById("input-form");
+    this.elementInput = document.getElementById("input-task");
+    this.elementError = document.querySelector(".error");
   }
 
   init() {
-    initialData(arrData);
+    initNotepad(arrData);
 
     displayForm.redrawTasks(arrData.tasks);
 
@@ -25,53 +23,54 @@ export default class WorkTasks {
   }
 
   eventsTasks() {
-    this.formInput.addEventListener('submit', (event) => {
-      event.preventDefault();
+    this.formInput.addEventListener("submit", e => {
+      e.preventDefault();
       const elementValue = this.elementInput.value;
 
-      if (elementValue === '') {
-        this.elementError.classList.remove('hidden');
-        const top = this.elementInput.offsetTop - this.elementError.offsetHeight;
+      if (elementValue === "") {
+        this.elementError.classList.remove("hidden");
+        const top =
+          this.elementInput.offsetTop - this.elementError.offsetHeight;
         this.elementError.style.top = `${top - 5}px`;
         return;
       }
 
-      if (!this.elementError.classList.contains('hidden')) {
-        this.elementError.classList.add('hidden');
+      if (!this.elementError.classList.contains("hidden")) {
+        this.elementError.classList.add("hidden");
       }
 
       arrData.addTask(this.elementInput.value);
-      this.elementInput.value = '';
+      this.elementInput.value = "";
       this.filterTask(this.elementInput.value);
     });
 
-    this.elementInput.addEventListener('input', () => {
+    this.elementInput.addEventListener("input", () => {
       this.filterTask(this.elementInput.value);
     });
 
-    this.divPined.addEventListener('click', (event) => {
-      if (event.target.className === 'checked') {
-        const lementId = event.target.closest('.item-task').dataset.id;
+    this.pined.addEventListener("click", e => {
+      if (e.target.className === "checked") {
+        const lementId = event.target.closest(".item-task").dataset.id;
         this.moveTask(lementId, false);
       }
     });
 
-    this.divAllTasks.addEventListener('click', (event) => {
-      if (event.target.className === 'checked') {
-        const elmentId = event.target.closest('.item-task').dataset.id;
+    this.allTasks.addEventListener("click", e => {
+      if (e.target.className === "checked") {
+        const elmentId = e.target.closest(".item-task").dataset.id;
         this.moveTask(elmentId, true);
       }
     });
 
-    this.elementError.addEventListener('click', (event) => {
-      if (event.target.className === 'close-error') {
-        this.elementError.classList.add('hidden');
+    this.elementError.addEventListener("click", e => {
+      if (e.target.className === "close-error") {
+        this.elementError.classList.add("hidden");
       }
     });
   }
 
   filterTask(value) {
-    const filtrArr = arrData.tasks.filter((item) => {
+    const filtrArr = arrData.tasks.filter(item => {
       const valueLowerCase = value.trim().toLowerCase();
       const trueName = item.name.toLowerCase().includes(valueLowerCase);
       return trueName || item.pined;
@@ -80,7 +79,9 @@ export default class WorkTasks {
   }
 
   moveTask(itemIdTask, pined) {
-    const idTask = arrData.tasks.findIndex((item) => item.id === Number(itemIdTask));
+    const idTask = arrData.tasks.findIndex(
+      item => item.id === Number(itemIdTask)
+    );
     arrData.tasks[idTask].pined = pined;
     this.filterTask(this.elementInput.value);
   }
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable class-methods-use-this */
-import ArrData from './ArrData.js';
-import DisplayForm from './DisplayForm.js';
-import initial
+import WorkTasks from './WorkTasks.js';
+
+const workTasks = new WorkTasks();
+workTasks.init();
